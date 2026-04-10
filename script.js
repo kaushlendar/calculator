56dc83d413e30659ea58892f1e69b66fd64c56c2
kaushlendar <kaushlendar9508594279@gmail.com>
2026-04-10 16:34:04 +0530

    calculator
    I built a fully responsive and interactive portfolio website to showcase my skills and projects. This project demonstrates my understanding of front-end development and basic Java programming.

---
+++ script.js
@@ -0,0 +1,12 @@
+function press(num){
+document.getElementById("display").value += num;
+}
+
+function calculate(){
+let result = document.getElementById("display").value;
+document.getElementById("display").value = eval(result);
+}
+
+function clearDisplay(){
+document.getElementById("display").value = "";
+}
